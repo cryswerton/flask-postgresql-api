ff0759fa0e7ad93308371c6657078c59ccb01203
cryswerton <cryswertons@gmail.com>
2022-03-27 11:05:57 -0300

    refactoring and all and CRUD methods all implemented and working with postgres

--- backend/app.py
+++ backend/app.py
@@ -8,25 +8,25 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:cwss1994@localhost/users'
 db = SQLAlchemy(app)
 
-class Event(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"Event: {self.name} and {self.email}"
+        return f"User: {self.name} and {self.email}"
 
     def __init__(self, name, email):
         self.name = name
         self.email = email
 
-def format_event(event):
+def format_user(user):
     return {
-        "id": event.id,
-        "name": event.name,
-        "email": event.email,
-        "created_at": event.created_at
+        "id": user.id,
+        "name": user.name,
+        "email": user.email,
+        "created_at": user.created_at
     }
 
 
@@ -35,46 +35,46 @@ def hello():
     return 'Hey!'
 # create a record
 @app.route('/users/new', methods=['POST'])
-def create_event():
+def create_user():
     name = request.json['name']
     email = request.json['email']
-    event = Event(name, email)
-    db.session.add(event)
+    user = User(name, email)
+    db.session.add(user)
     db.session.commit()
-    return format_event(event)
+    return format_user(user)
 
 # get all users
 @app.route('/users', methods=['GET'])
 def get_users():
-    users = Event.query.order_by(Event.id.asc()).all()
+    users = User.query.order_by(User.id.asc()).all()
     user_list = []
     for user in users:
-        user_list.append(format_event(user))
+        user_list.append(format_user(user))
     return {'users': user_list}
 
 # get a single user
 @app.route('/users/get/<id>', methods=['GET'])
 def get_user(id):
-    user = Event.query.filter_by(id=id).one()
-    formatted_event = format_event(user)
-    return {'user': formatted_event}
+    user = User.query.filter_by(id=id).one()
+    formatted_user = format_user(user)
+    return {'user': formatted_user}
 
 # delete an user
 @app.route('/users/delete/<id>', methods=['DELETE'])
 def delete_user(id):
-    user = Event.query.filter_by(id=id).one()
+    user = User.query.filter_by(id=id).one()
     db.session.delete(user)
     db.session.commit()
     return f'User (id: {id}) deleted succesfully!'
 
 @app.route('/users/update/<id>', methods=['PUT'])
 def update_user(id):
-    user = Event.query.filter_by(id=id)
+    user = User.query.filter_by(id=id)
     name = request.json['name']
     email = request.json['email']
     user.update(dict(name=name, email=email, created_at=datetime.utcnow()))
     db.session.commit()
-    return {'event': format_event(user.one())}
+    return {'user': format_user(user.one())}
 
 if __name__=='__main__':
     app.run()
